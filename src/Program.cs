853104fd66eba045ed91a034cfe4e4891b313db3
Rene Bentes Pinto <renebentes@yahoo.com.br>
2025-10-27 20:02:54 -0300

    feat: gere o banco a partir das migrações

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,12 @@
+using BugStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services
+    .AddDbContext<AppDbContext>(options => options.UseSqlite(connectionString));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
